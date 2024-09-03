71c78b3c9f0968a9115e2dfcd0d0e4a4984fe31f
hashanrath <hashanrathnayake12345@gmail.com>
2024-09-03 10:58:15 +0530

    updated

--- src/pages/Message.js
+++ src/pages/Message.js
@@ -1,55 +1,13 @@
-import React, { useState } from 'react';
-import "./message.css";
+import React from 'react';
+import Navi from '../components/Navi';
+import Sidebar from '../components/Sidebar';
+import './message.css';
 
 export default function Message() {
-    const [messages, setMessages] = useState([]);
-    const [messageInput, setMessageInput] = useState('');
-
-    const handleSend = () => {
-        const messageText = messageInput.trim();
-        if (messageText !== '') {
-            appendMessage(messageText, 'user');
-            setMessageInput(''); // Clear input field
-        }
-    };
-
-    const appendMessage = (text, sender) => {
-        const newMessage = {
-            text,
-            sender,
-            id: messages.length + 1, // Unique ID for each message
-        };
-        setMessages([...messages, newMessage]);
-    };
-
-    const handleKeyDown = (event) => {
-        if (event.key === 'Enter') {
-            handleSend();
-        }
-    };
-
-    return (
-        <div className="chat-container">
-            <div className="chat-body" id="chat-body">
-                {messages.map((message) => (
-                    <div key={message.id} className={`message ${message.sender}`}>
-                        {message.text}
-                    </div>
-                ))}
-            </div>
-            <div className="chat-footer">
-                <input
-                    type="text"
-                    id="message-input"
-                    placeholder="Type a message..."
-                    value={messageInput}
-                    onChange={(e) => setMessageInput(e.target.value)}
-                    onKeyDown={handleKeyDown}
-                />
-                <button id="send-button" onClick={handleSend}>
-                    Send
-                </button>
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <Navi/>
+      <Sidebar/>
+    </div>
+  );
 }
